f260ebc8b48a3a7faa64bdf96330bd8e0e25995b
Bollolin <monkeyacademybogota@gmail.com>
2024-10-14 21:51:50 -0500

    first contract

---
+++ wow_signal.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.24;  // I define compile version
+
+// I specify the contract logic
+contract wow_signal {
+string public greeting = "martians of educateth"; // I declare the content
+}
